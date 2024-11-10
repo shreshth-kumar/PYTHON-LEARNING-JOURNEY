f7004295a3c05bbb0f6bfe1362c4a920add22eeb
shreshth-kumar <rkkalash4321@gmial.com>
2024-11-10 12:06:57 +0530

    initial commit

---
+++ 01-basics/practice questions/question 6.py
@@ -0,0 +1,4 @@
+#WAP to find the occurence of '$' in a string
+
+b = "string"
+print(b.count("$"))
---
+++ 01-basics/theory/Conditional Statements.py
@@ -0,0 +1,21 @@
+light = "green"
+
+if(light == "red"):
+    print("stop") #indentation 
+elif(light == "green"):
+    print("go")
+elif(light == "yellow"):
+    print("get ready")
+print("end of code")
+
+num = 5 
+if(num > 2):
+    print("greater than 2")
+if(num > 3):
+    print("greater than 3") 
+
+age = 24
+if(age >=18):
+    print("can vote")
+else:
+    print("cannot vote")
