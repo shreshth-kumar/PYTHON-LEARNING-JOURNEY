5ebf8b86e4e170d9099da661dc6a12f177dbfa9f
shreshth-kumar <rkkalash4321@gmial.com>
2024-12-07 18:16:14 +0530

    initial commit

--- Python Series/practice questions/question 11.py
+++
@@ -1,7 +0,0 @@
-#WAP to ask the user to enter names of thier 4 favorite movies and store them in a list.
-
-a = input("enter your first favorite movie =")
-b = input("enter your second favorite movie =")
-c = input("enter your third favorite movie =")
-
-print([a, b, c])
---
+++ Python/practice questions/question 11.py
@@ -0,0 +1,13 @@
+#WAP to ask the user to enter names of thier 3 favorite movies and store them in a list.
+
+a = input("enter your first favorite movie =")
+b = input("enter your second favorite movie =")
+c = input("enter your third favorite movie =")
+
+movies = []
+
+movies.append(a)
+movies.append(b)
+movies.append(c)
+
+print(movies)
