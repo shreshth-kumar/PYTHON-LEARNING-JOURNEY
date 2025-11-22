577a83f1ba22e913fba6efcd926649525ed68559
shreshth-kumar <rkkalash4321@gmial.com>
2025-11-22 15:28:19 +0530

    learned Class

---
+++ Python/Basics/theory/Class.py
@@ -0,0 +1,16 @@
+class Student:
+    name = "karan"
+
+s1 = Student()
+print(s1.name)
+
+s2= Student()
+print(s2.name)
+
+class Car:
+    color = "blue"
+    brand = "mercedes"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
