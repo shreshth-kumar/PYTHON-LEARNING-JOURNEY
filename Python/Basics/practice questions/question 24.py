b5be021bf4712d3aaa11bb3a0b20b403721f3e1e
shreshth-kumar <rkkalash4321@gmial.com>
2026-01-06 00:20:40 +0530

    2nd part of the question

--- Python/Basics/practice questions/question 24.py
+++ Python/Basics/practice questions/question 24.py
@@ -1,5 +1,5 @@
 '''question 24: Write a program which repeatedly reads integers until the user enters “done”. Once “done” is entered, print out the total, count, and average of the integers.
-          If the user enters anything other than an integer, detect their mistake using try and except and print an error message and skip to the next integers.'''
+                If the user enters anything other than an integer, detect their mistake using try and except and print an error message and skip to the next integers.'''
 
 num = []
 
@@ -25,3 +25,24 @@ for i in num:
 avg = total / count
 
 print(total, count, avg)
+
+'''Exercise 2: Write another program that prompts for a list of numbers as above and at the end prints out both the maximum and minimum of the numbers instead of the 
+               average.'''
+
+def min(num):
+    smallest = None
+    for i in num :
+        if smallest is None or i < smallest:
+            smallest = i
+    return smallest
+
+print("smallest number: " ,min(num))
+
+def max(num):
+    largest = None
+    for i in num :
+        if largest is None or i > largest :
+            largest = i
+    return largest
+
+print("largest number: " ,max(num))
