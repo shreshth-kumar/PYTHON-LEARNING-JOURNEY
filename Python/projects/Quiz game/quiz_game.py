037d2d7ec388b3ddbc9fb69657417f7bf9242a46
shreshth-kumar <rkkalash4321@gmail.com>
2026-01-08 20:04:41 +0530

    build a Quiz program

---
+++ Python/projects/Quiz game/quiz_game.py
@@ -0,0 +1,54 @@
+# print("welcome to my Quiz!")
+
+# playing = input("Do you want to play the game? ")
+# playing = playing.lower()
+
+# if playing != "yes":
+#     quit()
+
+# print("Okay! Let's begin")
+score = 0
+
+# answer = input("1) What does CPU stand for? ")
+# answer = answer.lower()
+# if answer == "central processing unit":
+#     print('Correct!')
+#     score += 1
+# else:
+#     print('Incorrect!')
+
+
+# answer = input("2) What does GPU stand for? ")
+# answer = answer.lower()
+# if answer == "graphics processing unit":
+#     print('Correct!')
+#     score += 1
+# else:
+#     print('Incorrect!')
+
+# answer = input("3) What does PSU stand for? ")
+# answer = answer.lower()
+# if answer == "power supply unit":
+#     print('Correct!')
+#     score += 1
+# else:
+#     print('Incorrect!')
+
+answer = input("4) What does RAM stand for? ")
+answer = answer.lower()
+if answer == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print('Incorrect!')
+
+# answer = input("5) What does ROM stand for? ")
+# answer = answer.lower()
+# if answer == "read only memory":
+#     print('Correct!')
+#     score += 1
+# else:
+#     print('Incorrect!')
+
+
+# print("You got " + str(score) + " questions correct!")
