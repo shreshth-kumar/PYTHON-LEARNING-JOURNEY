fbdcf05dfddcf5501a2659554d4634b9e66bfcaa
shreshth-kumar <rkkalash4321@gmail.com>
2026-01-10 17:14:40 +0530

    added wpm_test function

--- Python/projects/10. WPM typing test/wpm_typing_test.py
+++ Python/projects/10. WPM typing test/wpm_typing_test.py
@@ -1,10 +1,46 @@
 import curses
 from curses import wrapper
 
-def main (stdscr):
+def start_screen(stdscr):
     stdscr.clear()
-    stdscr.addstr("Hello world!")
+    stdscr.addstr("Welcome to the Speed Typing Test!")
+    stdscr.addstr("\nPress any key to begin!")
     stdscr.refresh()
     stdscr.getkey()
+
+def wpm_test(stdscr):
+    target_text = "Hello world this is some test text for this app"
+    current_text = []
     
+    while True:
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+        if key in ("KEY_BACKSPACE", '\b', "\x7f"):
+            if len(current_text) > 0:
+                current_text.pop()
+        else:
+            current_text.append(key)
+
+        stdscr.clear()
+        stdscr.addstr(target_text)
+
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+
+            stdscr.refresh()
+    
+
+def main (stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    stdscr.clear()
+    stdscr.addstr(0, 0, "Hello world!")
+    stdscr.refresh()
+    
+    start_screen(stdscr)
+    wpm_test(stdscr)
+
 wrapper(main)
