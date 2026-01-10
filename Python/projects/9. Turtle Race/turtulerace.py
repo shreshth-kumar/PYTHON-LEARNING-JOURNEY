566dd0c7afce17c72b445dcbfcaecb8f70c52938
shreshth-kumar <rkkalash4321@gmail.com>
2026-01-10 14:32:12 +0530

    user input for number of turtle in race

---
+++ Python/projects/9. Turtle Race/turtulerace.py
@@ -0,0 +1,17 @@
+def get_number_of_racers():
+    racer = 0 
+    while True:
+        racers = input("Enter the number of racers ( 2 - 10): ")
+        if racers.isdigit():
+            racers = int(racers)
+        else:
+            print('Input is not numeric..... Try Again!')
+            continue
+        
+        if 2<= racers <= 10:
+            return racers
+        else:
+            print('number not in range 2-10. Try Again!')
+
+racers = get_number_of_racers()
+print(racers)
