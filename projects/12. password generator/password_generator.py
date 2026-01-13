074dbfc03efd3d0290e8fd1243d04646f4a0bfd9
shreshth-kumar <rkkalash4321@gmail.com>
2026-01-13 21:59:58 +0530

    completed the generate password program

---
+++ projects/12. password generator/password_generator.py
@@ -0,0 +1,43 @@
+import random
+import string
+
+def generate_password(min_length, numbers = True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+    
+    pwd = ""
+    meets_criteria = False
+    has_numbers = False
+    has_speacial = False
+
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_numbers = True
+        elif new_char in special:
+            has_speacial = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_numbers
+        if special_characters:
+            meets_criteria = meets_criteria and has_speacial
+
+    return pwd
+
+
+min_length = int(input("Enter the minimum length: "))
+has_numbers = input("Do you want to have numbers (y/n)?").lower() == 'y'
+has_special = input("Do you want to have special characters (y/n)?").lower() == 'y'
+pwd = generate_password(min_length, has_numbers, has_special)
+print("The generated password is:", pwd)
