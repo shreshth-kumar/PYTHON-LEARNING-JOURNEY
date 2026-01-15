c532eff7d65f5283d603287210d50b500d1ab5b1
shreshth-kumar <rkkalash4321@gmail.com>
2026-01-15 22:34:35 +0530

    _

--- projects/12. password generator/password_generator.py
+++ projects/12. password generator/password_generator.py
@@ -41,3 +41,5 @@ has_numbers = input("Do you want to have numbers (y/n)?").lower() == 'y'
 has_special = input("Do you want to have special characters (y/n)?").lower() == 'y'
 pwd = generate_password(min_length, has_numbers, has_special)
 print("The generated password is:", pwd)
+
+
