c864d4592b552256b4e74e3e5561c56788a5dde0
shreshth-kumar <rkkalash4321@gmail.com>
2026-01-16 22:51:41 +0530

    initialized a find start function

--- projects/13. Shortest Path Finder/shortest_path_finder.py
+++ projects/13. Shortest Path Finder/shortest_path_finder.py
@@ -1,5 +1,6 @@
 import curses
 from curses import wrapper
+import queue
 import time
 
 
@@ -23,7 +24,18 @@ def print_maze(maze, stdscr, path=[]):
         for j, value in enumerate(row):
             stdscr.addstr(i, j*2 , value, BLUE)
 
+def find_start(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j 
+            
+    return None
 
+def find_path(maze, stdscr):
+    start = "O"
+    end = "X"
+    start_pos = find_start(maze, start)
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
