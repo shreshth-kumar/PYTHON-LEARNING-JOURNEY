05f588655a8a70c8760b503af2478541cc1d63d2
shreshth-kumar <rkkalash4321@gmail.com>
2026-01-16 22:26:00 +0530

    initalized main funtion

--- projects/13. Shortest Path Finder/shortest_path_finder.py
+++ projects/13. Shortest Path Finder/shortest_path_finder.py
@@ -16,8 +16,12 @@ maze = [
 ]
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
+    blue_and_black = curses.color_pair(1)
+
+
     stdscr.clear()
-    stdscr.addstr(0, 0, "hello world!")
+    stdscr.addstr(0, 0, "hello world!", blue_and_black)
     stdscr.refresh()
     stdscr.getch()
     
