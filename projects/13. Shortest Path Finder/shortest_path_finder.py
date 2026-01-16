0067e25a4c1ce619a3d9e084293d48261a0a5768
shreshth-kumar <rkkalash4321@gmail.com>
2026-01-16 22:41:13 +0530

    initailzed a print main funtion

--- projects/13. Shortest Path Finder/shortest_path_finder.py
+++ projects/13. Shortest Path Finder/shortest_path_finder.py
@@ -15,13 +15,24 @@ maze = [
     ["#", "#", "#", "#", "#", "#", "#", "X", "#"]
 ]
 
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            stdscr.addstr(i, j*2 , value, BLUE)
+
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
-    blue_and_black = curses.color_pair(1)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    
 
 
     stdscr.clear()
-    stdscr.addstr(0, 0, "hello world!", blue_and_black)
+    print_maze(maze, stdscr)
     stdscr.refresh()
     stdscr.getch()
     
